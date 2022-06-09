e12a60dc104bfc0a725a2e88cea8a8cc638c329d
Felix Schekerka <felixschekerka@gmail.com>
2022-06-09 17:33:04 +0200

    adding greenscreen video.py

---
+++ greenscreen/green_screen_video.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+
+img = Image.open('greenscreen/mc_background.png')
+img.save('img.jpg')
+img = cv.imread('img.jpg')
+
+
+video = cv.VideoCapture('greenscreen/bateman_greenscreen.mp4')
+
+w = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
+h = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+while True:
+    retval, image = video.read()
+    if retval:
+        image = cv.resize(image, (w, h))
+        img = cv.resize(img, (w, h))
+        hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        lower_green = (38, 40, 30)
+        upper_green = (85, 255, 255)
+        mask = cv.inRange(hsv_img, lower_green, upper_green)
+        final = cv.bitwise_and(image, image, mask=mask)
+        f = image - final
+        f = np.where(f == 0, img, f)
+        cv.imshow('video', f)
+    else:
+        video.set(cv.CAP_PROP_POS_FRAMES, 0)
+        continue
+    if cv.waitKey(5) == ord('q'):
+        break
+video.release()
+cv.destroyAllWindows()
---
+++ meme_generator/README.md
@@ -0,0 +1,13 @@
+# Meme-Creator
+
+
+## Creating a Meme
+Loading up a Picture,and adding 2 Text-Fields to it
+
+## Usage:
+Input:
+![Input:](resources/img/3.jpg "Input")
+
+Output: 
+![Output:](meme_generator/output_meme.png "Output")
+
