37a3afa4eb17c26917224afc63a7792ed96a445c
Felix Schekerka <felixschekerka@gmail.com>
2022-06-10 09:14:11 +0200

    code adjustments

--- greenscreen/green_screen_video.py
+++ greenscreen/green_screen_video.py
@@ -1,34 +1,34 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from PIL import Image
 
 
 img = Image.open('greenscreen/mc_background.png')
 img.save('img.jpg')
-img = cv.imread('img.jpg')
+img = cv2.imread('img.jpg')
 
 
-video = cv.VideoCapture('greenscreen/bateman_greenscreen.mp4')
+video = cv2.VideoCapture('greenscreen/bateman_greenscreen.mp4')
 
-w = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
-h = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 while True:
     retval, image = video.read()
     if retval:
-        image = cv.resize(image, (w, h))
-        img = cv.resize(img, (w, h))
-        hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        image = cv2.resize(image, (w, h))
+        img = cv2.resize(img, (w, h))
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         lower_green = (38, 40, 30)
         upper_green = (85, 255, 255)
-        mask = cv.inRange(hsv_img, lower_green, upper_green)
-        final = cv.bitwise_and(image, image, mask=mask)
+        mask = cv2.inRange(hsv_img, lower_green, upper_green)
+        final = cv2.bitwise_and(image, image, mask=mask)
         f = image - final
         f = np.where(f == 0, img, f)
-        cv.imshow('video', f)
+        cv2.imshow('video', f)
     else:
-        video.set(cv.CAP_PROP_POS_FRAMES, 0)
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
         continue
-    if cv.waitKey(5) == ord('q'):
+    if cv2.waitKey(5) == ord('q'):
         break
 video.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
